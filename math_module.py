b30e6ccd6112f6a4191587b34dbb4c9d02a09eee
mukhamedzhanov.amir <amir8919@mail.ru>
2023-09-16 18:59:29 +0500

    added new func in math_module.py

--- math_module.py
+++ math_module.py
@@ -1,6 +1,14 @@
 import math as m
 
+
 def calc_circle(r):
-    return m.pi * (r**2)
+    return m.pi * (r ** 2)
+
 
 print(calc_circle(4))
+
+
+def calc_len_circle(r):
+    return 2 * m.pi * r
+
+print(calc_len_circle(4))
